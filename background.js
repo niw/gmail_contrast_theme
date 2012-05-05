4f3f8cedfa96476e1eb63219ee1b6961c2b25538
Yoshimasa Niwa <niw@niw.at>
2012-05-05 02:43:20 -0700

    Add options.

---
+++ background.js
@@ -0,0 +1,29 @@
+!function() {
+  "use strict"
+
+  var setup = function(tab) {
+    var options = JSON.parse(localStorage["options"] || "{}")
+    var csses = [
+      "new_look.css"
+    ]
+    for(var i in options) {
+      console.debug(i)
+      if(options[i]) {
+        csses.push(i + ".css")
+      }
+    }
+    for(var i = 0, l = csses.length; i < l; i++) {
+      chrome.tabs.insertCSS(tab.id, {
+        file: csses[i],
+        allFrames: true
+      })
+    }
+  }
+
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    var tab = sender.tab
+    if(tab && request.injected) {
+      setup(tab)
+    }
+  })
+}()
---
+++ content.js
@@ -0,0 +1,4 @@
+!function() {
+  "use strict"
+  chrome.extension.sendRequest({injected: true})
+}()
